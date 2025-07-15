fd79c1d6de29f48d91518be36ee95f104350edee
pedro2806 <isc.pedro.mtz@gmail.com>
2025-07-15 12:59:56 -0600

    Se sube version funcional para incidencias
    Se sube version funcional para incidencias

--- encabezado.php
+++ encabezado.php
@@ -48,7 +48,7 @@
     <li class = "nav-item dropdown no-arrow">
         <a class = "nav-link dropdown-toggle" href = "#" id = "userDropdown" role = "button"
             data-toggle = "dropdown" aria-haspopup = "true" aria-expanded = "false">
-            <span class = "mr-2 d-none d-lg-inline text-gray-600 small">
+            <span class = "mr-4 text-gray-600">
                 <?php echo $_COOKIE['nombredelusuario']?>
             </span>
             
