dd23cc8469f848bdf3d307f88b72c766fade40cf
pedro2806 <isc.pedro.mtz@gmail.com>
2025-10-20 17:30:04 -0600

    Update cambiar_contrasena.php

--- cambiar_contrasena.php
+++ cambiar_contrasena.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-include '../incidencias/conn.php';
+include '../controlVehicular/conn.php';
 
-$id_usuario = $_COOKIE['id_usuario'];
+$id_usuario = $_COOKIE['noEmpleadoL'] ?? '';
 
 $contrasena_actual = $_POST['contrasena_actual'] ?? '';
 $nueva_contrasena = $_POST['nueva_contrasena'] ?? '';
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     // Consultar la contraseña actual en la BD
-    $stmt = $conn->prepare("SELECT password FROM usuarios WHERE id_usuario = $id_usuario");
+    $stmt = $conn->prepare("SELECT password FROM usuarios WHERE noEmpleado = $id_usuario");
     $stmt->execute();
     $stmt->bind_result($password_en_bd);
     $stmt->fetch();
@@ -25,16 +25,24 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Verificar la contraseña actual (sin hash)
     if ($contrasena_actual !== $password_en_bd) {
-        echo "La contraseña actual es incorrecta.";
+        //echo "La contraseña actual es incorrecta.";
+        echo "<script>   
+                    alert('La contraseña actual es incorrecta.');
+                    window.location.href = 'inicio.php';                
+                </script>";
         exit;
     }
 
     // Actualizar la contraseña nueva en la BD (sin hash)
-    $stmt = $conn->prepare("UPDATE usuarios SET password = ? WHERE id_usuario = $id_usuario");
+    $stmt = $conn->prepare("UPDATE usuarios SET password = ? WHERE noEmpleado = $id_usuario");
     $stmt->bind_param("s", $nueva_contrasena);
 
     if ($stmt->execute()) {
-        echo "Contraseña actualizada correctamente.";
+        //echo "Contraseña actualizada correctamente.";
+        echo "<script>
+                    alert('Contraseña actualizada correctamente.');
+                    window.location.href = 'inicio.php';                
+                </script>";
     } else {
         echo "Error al actualizar la contraseña.";
     }
