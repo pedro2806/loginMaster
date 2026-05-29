5f2b776c350438edccce43a92c3d0b1225fef69e
IamDef4lt <amraml8a@gmail.com>
2026-05-29 15:13:13 -0600

    Update index.php

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action']) && $_POST['
         $mail->SMTPAuth   = true;
         $mail->Username   = 'mess.metrologia@gmail.com';
         $mail->Password   = 'hglidvwsxcbbefhe';
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->SMTPSecure = 'ssl'; //PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 465; //desarrollo 587, produccion 465 con SSL
         $mail->CharSet    = 'UTF-8';
         
